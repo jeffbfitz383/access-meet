2cb50e25edb563ee1bc5b6e69100761b0890d93f
Jeffrey <jefbfitzp33@outlook.com>
2024-07-22 14:08:50 -0600

    add db json

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("index.js works")
+//const fetch = require('node-fetch');
+
+console.log("index.js works");
+
+fetch("http://localhost:3000/events")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error fetching events:', error);
+  }); 
